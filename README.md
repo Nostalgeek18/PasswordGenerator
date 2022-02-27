f88d7d6426688ef9ff4a05467f0c0ae47194a5ae
Nazim Mouzai <mouzai.n@yahoo.com>
2022-02-27 12:23:41 -0500

    Finishing first version

---
+++ README.md
@@ -0,0 +1,3 @@
+Web page to generate random passwords created from scratch.
+
+Challange made as part of Scrimba's courses 
--- index.js
+++ index.js
@@ -1,28 +1,32 @@
-const passwordsInput = document.querySelectorAll(".password");
-const generateBtn    = document.querySelector(".password-btn");
-const copyTxt        = document.querySelector(".copied");
+const passwordsInput    = document.querySelectorAll(".password");
+const generateBtn       = document.querySelector(".password-btn");
+const copyTxt           = document.querySelector(".msgUser");
+const passLengthDisplay = document.querySelector(".password-length");
 
+const arrowUp      = document.querySelector(".fa-circle-up");
+const arrowDown    = document.querySelector(".fa-circle-down");
 
+let lengthPassword = parseInt(passLengthDisplay.textContent);
 /*************************FUNCTIONS ********************************/
 
 function randomPassword() {
-    let length = 10;
+    let length = lengthPassword;
     let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%?&*()_+@{[]}|°/.:^<>";
     let retVal = "";
     for (let i = 0, n = charset.length; i < length; ++i) {
         retVal += charset.charAt(Math.floor(Math.random() * n));
     }
+
+    passLengthDisplay.textContent = length;
     return retVal;
 }
 
+//Fill inputs
 function fillPasswordInput(){
     passwordsInput.forEach(password => {
         password.value = randomPassword();
     })
 }
-
-
-
 
 //************************EVENT LISTENERS ******************************/
 
@@ -39,7 +43,26 @@ passwordsInput.forEach(input => {
     input.addEventListener('click', ()=> {
         input.readOnly = true;
         navigator.clipboard.writeText(input.value);
+        copyTxt.textContent = 'Password copied.'
         copyTxt.style.display = 'block';
     })
+})
+
+arrowDown.addEventListener('click', ()=> {
+    if(lengthPassword >4){
+        lengthPassword--;
+        fillPasswordInput();
+        copyTxt.textContent = "";
+    }else {
+        copyTxt.textContent = "Can't have password below 4 characters !";
+        copyTxt.style.display = 'block';
+    }
+})
+
+
+arrowUp.addEventListener('click', ()=> {
+    copyTxt.textContent = "";
+    lengthPassword++;
+    fillPasswordInput();
 })
 
